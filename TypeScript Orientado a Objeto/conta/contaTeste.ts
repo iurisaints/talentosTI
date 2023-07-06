91e713bd9502538c0214db8eef8cb5cb9b9911f5
Iuri Santos <50501178+iurisaints@users.noreply.github.com>
2023-07-06 08:50:59 -0300

    Create contaTeste.ts

---
+++ TypeScript Orientado a Objeto/conta/contaTeste.ts
@@ -0,0 +1,9 @@
+import { ContaBancaria } from "./conta";
+import * as rl from "readline-sync";
+
+const nomeConta = rl.question("Qual o nome do proprietário da conta?: ");
+const numeroConta = rl.question("Qual o número da conta?: ");
+
+const contaNova = new ContaBancaria(nomeConta, numeroConta);
+
+console.log(contaNova);
